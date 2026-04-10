ef3dbc06c8fdb476f27bdc934b98f4a8f75bbd47
Bekzhan <bekzhan.tanirbergen@narxoz.kz>
2026-04-10 18:41:13 +0500

    add boss phase three combat strategy

---
+++ src/com/narxoz/rpg/strategy/BossPhaseThreeStrategy.java
@@ -0,0 +1,8 @@
+package com.narxoz.rpg.strategy;
+
+public class BossPhaseThreeStrategy extends ScaledCombatStrategy {
+
+    public BossPhaseThreeStrategy() {
+        super("Cataclysmic Frenzy", 1.8, 0.2);
+    }
+}
