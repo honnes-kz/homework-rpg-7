0b03856c771db39a9c1c26847b5b523eb143b287
Bekzhan <bekzhan.tanirbergen@narxoz.kz>
2026-04-10 18:39:51 +0500

    add boss phase one combat strategy

---
+++ src/com/narxoz/rpg/strategy/BossPhaseOneStrategy.java
@@ -0,0 +1,8 @@
+package com.narxoz.rpg.strategy;
+
+public class BossPhaseOneStrategy extends ScaledCombatStrategy {
+
+    public BossPhaseOneStrategy() {
+        super("Measured Tyranny", 1.0, 1.2);
+    }
+}
