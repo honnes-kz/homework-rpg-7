eb71a6f7e29e4a92de050c5d06df78d6b8dcba66
Bekzhan <bekzhan.tanirbergen@narxoz.kz>
2026-04-10 13:36:55 +0500

    add aggressive hero combat strategy

---
+++ src/com/narxoz/rpg/strategy/AggressiveStrategy.java
@@ -0,0 +1,8 @@
+package com.narxoz.rpg.strategy;
+
+public class AggressiveStrategy extends ScaledCombatStrategy {
+
+    public AggressiveStrategy() {
+        super("Berserker Rush", 1.5, 0.7);
+    }
+}
