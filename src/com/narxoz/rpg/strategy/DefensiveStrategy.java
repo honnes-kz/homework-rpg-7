2be6e0cc42ac35edab1cf6ec12439c4d770abe16
Bekzhan <bekzhan.tanirbergen@narxoz.kz>
2026-04-10 13:37:17 +0500

    add defensive hero combat strategy

---
+++ src/com/narxoz/rpg/strategy/DefensiveStrategy.java
@@ -0,0 +1,8 @@
+package com.narxoz.rpg.strategy;
+
+public class DefensiveStrategy extends ScaledCombatStrategy {
+
+    public DefensiveStrategy() {
+        super("Iron Guard", 0.8, 1.5);
+    }
+}
