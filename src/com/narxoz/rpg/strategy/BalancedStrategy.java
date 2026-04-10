22337b1368ac50ec5488b24e5bd2d9a3c5f062e6
Bekzhan <bekzhan.tanirbergen@narxoz.kz>
2026-04-10 13:36:25 +0500

    add balanced hero combat strategy

---
+++ src/com/narxoz/rpg/strategy/BalancedStrategy.java
@@ -0,0 +1,8 @@
+package com.narxoz.rpg.strategy;
+
+public class BalancedStrategy extends ScaledCombatStrategy {
+
+    public BalancedStrategy() {
+        super("Balanced Stance", 1.0, 1.0);
+    }
+}
