5ca56f3a4e28ef005e49a9b5e4379d67a28b6b0f
Bekzhan <bekzhan.tanirbergen@narxoz.kz>
2026-04-10 18:40:33 +0500

    add boss phase two combat strategy

---
+++ src/com/narxoz/rpg/strategy/BossPhaseTwoStrategy.java
@@ -0,0 +1,8 @@
+package com.narxoz.rpg.strategy;
+
+public class BossPhaseTwoStrategy extends ScaledCombatStrategy {
+
+    public BossPhaseTwoStrategy() {
+        super("Ravaging Assault", 1.35, 0.9);
+    }
+}
